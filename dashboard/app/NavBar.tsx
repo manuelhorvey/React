5ea56e83f6dcd467061f351cb8770e5300112f7d
manuelhorvey <ameymanuel@gmail.com>
2023-11-22 00:47:27 +0000

    Initial commit

---
+++ dashboard/app/NavBar.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React, { use } from 'react'
+import { AiFillBug } from "react-icons/ai";
+import classNames from 'classnames';
+
+const NavBar = () => {
+
+    const currentPath = usePathname();
+    const links = [
+        {label: 'Dashboard', href :'/'}, 
+        {label: 'Issues', href : '/issues'}, 
+    ];
+  return (
+    <nav className='flex space-x-6 border-b mb-5 padding-5 px-2 h-12 items-center hover:bg-slate-200'>
+        <Link href="/"><AiFillBug /></Link>
+        <ul className='space-x-6 flex'>
+            {links.map(link => 
+            <Link key={link.href} 
+            className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !==  currentPath,
+                'hover:text-zinc-800 transion-colors' : true
+            })}
+            href={link.href}>{link.label} </Link> )}
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ dashboard/app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssusesPage = () => {
+  return (
+    <div>IssusesPage</div>
+  )
+}
+
+export default IssusesPage
